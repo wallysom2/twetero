274b29b97f39e75b202a3efad887efe82f6c4beb
Wallyson <wallysom2@gmail.com>
2022-08-29 17:27:24 -0300

    criando rota para mostrar tweets

--- index.js
+++ index.js
@@ -5,44 +5,42 @@ const app = express();
 app.use(cors());
 app.use(json());
 
-let users = [
-  {
-    username: 'bobesponja', 
-    avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info" 
-  }
-];
+let users = [];
 
-let tweets = [
-  {
-    username: "bobesponja",
-    tweet: "eu amo o hub"
-  }
-]
+let tweets = []
 
 app.post('/sign-up', (req, res) => {
-  const newUser = req.body;
-  users.push({
-    ...newUser,
-    id: users.length + 1
-  });
-  res.send("OK");
+  const { username, avatar } = req.body;
+  
+  if (!username || !avatar) {
+    return res.status(400).send("Todos os campos são obrigatórios!");
+  } else {
+    users.push({ username, avatar });
+    res.status(201).send("OK");
+  }
 });
 
 app.post('/tweets', (req, res) => {
-  const newTweet = req.body;
-  tweets.push({
-    ...newTweet,
-    id: tweets.length + 1
-  });
-  res.send("OK");
-});
+  const { tweet } = req.body;
+  const { user:username } = req.headers;
+  if (!username || !tweet) {
+    return res.status(400).send("Todos os campos são obrigatórios!");
+  } else {
+    tweets.push({
+      username,
+      avatar: users.find((user) => user.username === username).avatar,
+      tweet,
+    });
+    res.status(201).send("OK");
+  }
+});;
 
 app.get('/tweets', (req, res) => {
   res.send(tweets);
 });
 
 
-app.listen(5000, () => console.log("Server is running on port 5000."));
+app.listen(5002, () => console.log("Server is running on port 5000."));
 
 
  
